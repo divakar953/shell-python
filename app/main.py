6fd9238ba5ace38717b6b444fa6a408ef79fc9ca
Divakar Reddy Vanga <divakarvanga@gmail.com>
2026-08-26 13:32:08 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,12 +4,15 @@ import sys
 def main():
     while True:
         command = input("$ ")
-        command = command[5:]
-        if command == "exit" or command == "type" or command == "echo":
-            print(f"{command} is a shell builtin")
+        if command.startswith("echo"):
+            print(command[5:])
         else:
-            print(f"{command}: not found")
-
+            command = command[5:]
+            if command == "exit" or command == "type" or command == "echo":
+                print(f"{command} is a shell builtin")
+            else:
+                print(f"{command}: not found")
+        
 
 if __name__ == "__main__":
     main()
