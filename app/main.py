758e29449af7dc063f249b9c6d56e578cb11d0ee
Divakar Reddy Vanga <divakarvanga@gmail.com>
2026-08-26 13:33:44 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,13 +6,14 @@ def main():
         command = input("$ ")
         if command.startswith("echo"):
             print(command[5:])
-        else:
+        elif command.startswith("type"):
             command = command[5:]
             if command == "exit" or command == "type" or command == "echo":
                 print(f"{command} is a shell builtin")
             else:
                 print(f"{command}: not found")
-        
+        else:
+            print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
